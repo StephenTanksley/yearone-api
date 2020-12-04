acd7c61beea8912e6535dd2aeb5c49e81127e6f7
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-12-04 15:28:06 -0600

    feat: Wrote model functions to interact with db.

--- data/routes/filmsModel.js
+++ data/routes/filmsModel.js
@@ -7,3 +7,38 @@ const get = () => {
 const getBy = (filter) => {
   return db("favorites").where(filter).first();
 };
+
+const getById = async (id) => {
+  const film = await db("films")
+    .where({ imdb_id: id })
+    .select("id", "title", "imdb_id", "image_url", "thumbs_up", "thumbs_down")
+    .first();
+
+  return film;
+};
+
+const add = (film) => {
+  return db("films").insert(film).returning("*");
+};
+
+const update = (id, changes) => {
+  return db("films")
+    .where({
+      imdb_id: id,
+    })
+    .update(changes)
+    .returning("*");
+};
+
+const remove = (id) => {
+  return db("films").where({ imdb_id: id }).del();
+};
+
+module.exports = {
+  get,
+  getBy,
+  getById,
+  add,
+  update,
+  remove,
+};
