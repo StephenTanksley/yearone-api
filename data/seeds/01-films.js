55b36cb8b1f88cef146b4fa65b695d801e3d75a0
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-12-04 15:20:33 -0600

    feat: Added seed files.

--- data/seeds/01-films.js
+++ data/seeds/01-films.js
@@ -1,13 +1,12 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function (knex) {
+  await knex("films").insert([
+    {
+      title: "The Batman",
+      imdb_id: "tt1877830",
+      image_url:
+        "https://m.media-amazon.com/images/M/MV5BZTE2YTY3YTMtM2FlMS00YmI3LTgyMWUtM2FhMWIyZWRmMDk5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@.jpg",
+      thumbs_up: 12765027,
+      thumbs_down: 23093,
+    },
+  ]);
 };
